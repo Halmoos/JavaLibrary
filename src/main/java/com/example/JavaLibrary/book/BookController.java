8e39cb0ad4b807bf81d718975554bd3deae21074
Haytham <haytham-an@hotmail.com>
2024-02-28 13:18:26 +0300

    feat: CQRS added to controller

--- src/main/java/com/example/JavaLibrary/book/BookController.java
+++ src/main/java/com/example/JavaLibrary/book/BookController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "api/book")
 public class BookController {
 
