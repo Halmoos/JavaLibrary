7d50e78a9361b56cb49165bc51446137c75a63a4
Haytham <haytham-an@hotmail.com>
2024-03-18 15:26:05 +0300

    feat: appspec.yml file

---
+++ scripts/basic_health_check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Example: Basic health check
+echo "Performing health check..."
---
+++ scripts/start_application.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Starting application..."
+java -jar /tmp/JavaLibrary.jar >/dev/null 2>&1 &
